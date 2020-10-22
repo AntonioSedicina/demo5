aeec0cb2693846fb996f311eef7a5847e54c6328
Antonio Sedicina <user24xyz@gmail.com>
2020-10-22 19:17:56 +0200

    corona

---
+++ mavenproject1/src/main/java/NewClass.java
@@ -0,0 +1,16 @@
+public class NewClass {
+    /** non so cosa sto facendo
+     * @param args
+     */
+    public static void main (String[] args){
+        int n = 4;
+        System.out.println ("facciamo " + n + " chiacchiere");
+        String a = "scemo ";
+        String b = new String ("chi legge");
+        System.out.println (a+b);
+        int c = 20;
+        int d = c+2;
+        System.out.println ("la risposta a tutto quanto è "+(c+d));
+        System.out.println ("la pandemia finirà nel "+c+d);
+    }
+}
